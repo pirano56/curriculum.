93126e2f44113f6db3063956e21eae04b68bf985
pirano56 <masmasschecter@gmail.com>
2021-01-16 00:02:13 +0900

    2-5 課題提出

--- JavaStudy/2-5/src/study/Study.java
+++ JavaStudy/2-5/src/study/Study.java
@@ -3,6 +3,7 @@ package study;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 本課題では、コレクションフレームワークのList系クラスとMap系クラスの基本的な記述を学びましょう。
@@ -21,35 +22,29 @@ public class Study {
 
 	public static void main(String args[]) {
 
-        // ① 定数を全て使って、String型のListを記述してください。
-    	List<String> shohinList = new ArrayList<String>();
-    	shohinList.add("バナナ");
-    	shohinList.add("牛乳");
-    	shohinList.add("豚肉");
-    	shohinList.add("コロッケ");
+		// ① 定数を全て使って、String型のListを記述してください。
+		List<String> shohinList = new ArrayList<String>();
+		shohinList.add("バナナ");
+		shohinList.add("牛乳");
+		shohinList.add("豚肉");
+		shohinList.add("コロッケ");
 
+		// ② 以下の「shopMap.put(shohinList.get(1), 180);」の処理について、コメントを記述してください。
+		/*
+		 * [ここへ記述]リストを記述した際の、shouhinList配列1番目の”牛乳”をキーにして、”牛乳”をキーにした際180という値が保持される。
+		 *
+		 */
+		LinkedHashMap<String, Integer> shopMap = new LinkedHashMap<String, Integer>();
+		shopMap.put(shohinList.get(0), 125);
+		shopMap.put(shohinList.get(1), 180);
+		shopMap.put(shohinList.get(2), 350);
+		shopMap.put(shohinList.get(3), 100);
 
+		// ③ カリキュラムを参考に拡張for文を使って、課題の画像と同じ表示になるよう記述してください。
+		// 「shohinList」と「shopMap」が保持する値を上手く利用しましょう。
+		for (Map.Entry<String, Integer> entry : shopMap.entrySet()) {
+			System.out.println(entry.getKey() +"="+ entry.getValue() + "円になります！");
+		}
 
-        // ② 以下の「shopMap.put(shohinList.get(1), 180);」の処理について、コメントを記述してください。
-        /*
-         * [ここへ記述]リストを記述した際の、shouhinList配列1番目の”牛乳”をキーにして、”牛乳”をキーにした際180という値が保持される。
-         *
-         */
-        LinkedHashMap<String, Integer> shopMap = new LinkedHashMap<String, Integer>();
-        shopMap.put(shohinList.get(0), 125);
-        shopMap.put(shohinList.get(1), 180);
-        shopMap.put(shohinList.get(2), 350);
-        shopMap.put(shohinList.get(3), 100);
-
-        // ③ カリキュラムを参考に拡張for文を使って、課題の画像と同じ表示になるよう記述してください。
-        // 「shohinList」と「shopMap」が保持する値を上手く利用しましょう。
-
-        for(Entry<String, String>entry:shopMap.entrySet()) {
-        	System.out.println(shopMap.getKey()+"="+shopMap.getValue()+"円になります！");
-
-        }
-
-
-
-    }
+	}
 }
