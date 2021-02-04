074bf0ca214b3b33379d735914ef124a074e3f21
pirano56 <masmasschecter@gmail.com>
2021-02-04 14:01:07 +0900

    2-6課題提出

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,18 @@
+package check;
+
+public class Check {
+
+	private String firstName="hirano";
+	private String lastName="masaru";
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	private String printName(String firstName, String lastName) {
+		return firstName + " " + lastName;
+
+	}
+
+}
